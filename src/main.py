3f74d93bbe91f9479353af924d197fd7d114946f
Lraxer <792296704@qq.com>
2024-07-12 22:02:20 +0800

    fix collecting abstract that containing equation
    write raw abstract to file

--- src/main.py
+++ src/main.py
@@ -85,7 +85,7 @@ def collect_abstract_impl(
             )
         tmp_library = bibtexparser.parse_string(entry_metadata[2])
         if abstract is not None:
-            abstract_field = bibtexparser.model.Field("abstract", abstract)
+            abstract_field = bibtexparser.model.Field("abstract", repr(abstract))
             tmp_library.entries[0].set_field(abstract_field)
         else:
             logger.warning(
