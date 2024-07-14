64c8704cbd8c4be66e97b515eaa5d41219de5e4a
Lraxer <792296704@qq.com>
2024-07-14 23:58:05 +0800

    refactor abstract collection

--- src/entry_usenix_conf.py
+++ src/entry_usenix_conf.py
@@ -1,9 +1,6 @@
 import requests
-from time import sleep
-from conf import req_headers
-from bs4 import BeautifulSoup
 import logging
-from request_wrap import make_request
+from get_abstract_base import get_abstract_base
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -15,32 +12,11 @@ logger.addHandler(handler)
 
 
 def get_full_abstract(abs_session: requests.Session, url: str, req_itv: float) -> str:
-    abstract = None
+    # 有两个一样的 `div.field...`，选取第二个标签，
+    # 以及最后需要选取全部的p标签
+    css_selector = "div.content > div.field.field-name-field-paper-description.field-type-text-long.field-label-above:nth-child(2) > div.field-items > div.field-item.odd > p"
 
-    if url == "":
-        return None
-
-    sleep(req_itv)
-
-    res = make_request(abs_session, url, headers=req_headers)
-    if res.status_code != 200:
-        logger.warning(
-            "Cannot access {}, status code: {}.".format(url, res.status_code)
-        )
-    else:
-        abs_soup = BeautifulSoup(res.text, "html.parser")
-        # 有两个一样的 `div.field...`，选取第二个标签，
-        # 以及最后需要选取全部的p标签
-        abs_tags = abs_soup.select(
-            "div.content > div.field.field-name-field-paper-description.field-type-text-long.field-label-above:nth-child(2) > div.field-items > div.field-item.odd > p"
-        )
-        if abs_tags != []:
-            abstract = " ".join([abs_tag.get_text() for abs_tag in abs_tags])
-
-        # 额外检查，可能是多余的
-        if abstract == "":
-            abstract = None
-
+    abstract = get_abstract_base(abs_session, url, req_itv, css_selector)
     return abstract
 
 
