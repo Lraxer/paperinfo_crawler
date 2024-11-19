bdd12a59dcd05f44e0ed4bb9f076aa85a1107488
Lraxer <792296704@qq.com>
2024-11-19 20:01:11 +0800

    change wait time to 10 seconds

--- src/entry_elsevier.py
+++ src/entry_elsevier.py
@@ -40,7 +40,7 @@ def get_abs_impl(url: str, driver) -> str:
     # 访问目标网页
     driver.get(url)
     # 等待最多10秒
-    wait = WebDriverWait(driver, 20)
+    wait = WebDriverWait(driver, 10)
     abs_tag = wait.until(
         EC.presence_of_element_located(
             (By.CSS_SELECTOR, "div.abstract.author > div > div")
