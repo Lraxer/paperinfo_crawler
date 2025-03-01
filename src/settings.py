14cfebcb5a4e035d18475bdd6ed5e16937b650cc
Lraxer <792296704@qq.com>
2025-03-01 18:15:20 +0800

    update UA

--- src/settings.py
+++ src/settings.py
@@ -8,7 +8,7 @@ cookie_path = "D:/pycode/chromedriver-user-data/"
 dblp_url = "https://dblp.org/db/"
 
 # User agent settings
-user_agent = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+user_agent = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
 
 # ACM header for requests
 req_headers = {
